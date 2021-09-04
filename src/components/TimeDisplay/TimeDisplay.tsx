cb6bc9696dc1a7643a195fbe3a9ce42e09a6e000
TCsTheMechanic <ceruttioliveira@hotmail.com>
2021-09-04 14:31:14 -0300

    Create timeDisplay component

---
+++ src/components/TimeDisplay/TimeDisplay.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { Text } from 'react-native';
+import { timeDisplayStyles } from './timeDisplayStyles';
+
+interface TimeDisplayProps {
+  buttonStatus: string
+}
+
+const TimeDisplay: React.FC<TimeDisplayProps> = ({ buttonStatus }) => {
+  const [seconds, setSeconds] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [hours, setHours] = useState(0)
+
+  useEffect(() => {
+    if (buttonStatus === 'stop') {
+      setTimeout(() => setSeconds (seconds + 1), 1000)
+    } else {
+      setSeconds(0)
+      setMinutes(0)
+      setHours(0)
+    }
+
+    if (seconds === 60) {
+      setSeconds(0)
+      setMinutes(minutes + 1)
+    }
+
+    if (minutes === 60) {
+      setMinutes(0)
+      setHours(hours + 1)
+    }
+  })
+
+  function numberPattern(number: number) {
+    return String(number).length != 2 ? '0' + number : number
+  }
+
+  return (
+    <Text style={timeDisplayStyles.time}>
+      {numberPattern(hours)}:{numberPattern(minutes)}:{numberPattern(seconds)}
+    </Text>
+  );
+}
+
+export default TimeDisplay;
---
+++ src/components/TimeDisplay/timeDisplayStyles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+import { theme } from '../../assets/styles/theme'
+
+export const timeDisplayStyles = StyleSheet.create({
+  time: {
+    color: theme.colors.text,
+    fontSize: 80,
+    marginBottom: 20,
+  }
+})
